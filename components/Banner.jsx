ee5374d09ca5c71a91286085efc10a875838cf85
spriveen <rpeanuka@gmail.com>
2025-07-01 06:55:30 -0700

    update banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,10 +12,10 @@ const Banner = () => {
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Level Up Your Gaming Experience
+          Upgrade Your Gameplay
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          From immersive sound to precise controls—everything you need to win
+         Experience total control and immersive sound—built for champions.
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
           Buy now
